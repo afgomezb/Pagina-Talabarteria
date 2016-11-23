ec6fe097326a75f0a5aad01204d303299fffc85a
Johan Gallo <johanfer94@hotmail.com>
2016-11-23 11:00:03 -0500

    se creo un user de prueba y se quito el boton de regitrar

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161109041705) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "indicators", force: :cascade do |t|
     t.string   "beneficiary"
     t.string   "impact"
@@ -69,8 +72,8 @@ ActiveRecord::Schema.define(version: 20161109041705) do
     t.string   "permission_level"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating User'
+
+User.destroy_all
+User.create!([
+  {email: "hugoramirezvelez@gmail.com", password: "talabarteriaapp", password_confirmation: "talabarteriaapp")}
+])
